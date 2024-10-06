ab8c6422075cea4dd24e8311643dab61b5a4161a
Khusein <Khusein@gmail.com>
2024-10-06 20:21:02 +0300

    added css module

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,31 +1,29 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { loadTodos } from '../redux/actions'
-import { useParams } from 'react-router-dom'
-import { Todo } from './Todo'
-import styles from "./Todos.module.css"
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadTodos } from "../redux/actions";
+import { useParams } from "react-router-dom";
+import { Todo } from "./Todo";
+import styles from "./Todos.module.css";
 
 export const Todos = () => {
-    const todos = useSelector((state) => state.todos.todos)
-    const dispatch = useDispatch()
-    const {id} = useParams()
+  const todos = useSelector((state) => state.todos.todos);
+  const dispatch = useDispatch();
+  const { id } = useParams();
 
-    useEffect(() => {
-        if (id !== undefined) {
-            return (
-                dispatch(loadTodos(id))
-            )
-        }
-    }, [id])
+  useEffect(() => {
+    if (id !== undefined) {
+      return dispatch(loadTodos(id));
+    }
+  }, [id]);
 
-  return (
-    <ul className={styles.todosUl}>
-        {
-            todos.map((todo) => {
-                return <Todo todo={todo} key={todo.id}/>
-            })
-        }
-    </ul>
-  )
-}
+  if (id !== undefined) {
+    return (
+      <ul className={styles.todosUl}>
+        {todos.map((todo) => {
+          return <Todo todo={todo} key={todo.id} />;
+        })}
+      </ul>
+    );
+  }
+  return <h3>⬅ Выберите пользователя</h3>;
+};
